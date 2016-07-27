7e86a36064f89332baf750eedc13cabb4d83b529
Ingomar Otter <iotter@mac.com>
2016-07-27 19:27:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ if [ -e $LOCAL_CONFIG_FILE ]; then rm $LOCAL_CONFIG_FILE; fi;
 if [ "$REMOTE_CONFIG" = true ] ; then
     # Get remote configuration repo
     if [ ! -d gateway-remote-config ]; then
-        git clone https://github.com/ttn-zh/gateway-remote-config.git
+        git clone https://github.com/ffimsl/ttn-gateway-remote-config.git
         pushd gateway-remote-config
     else
         pushd gateway-remote-config
